0ae63c5e7dc5037ec5487bfebb46a8713d51d9d0
Catherine Pulman <159820305+Clatherine@users.noreply.github.com>
2024-08-20 22:49:13 +0100

    got navbar toggle function to work correctly

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState , useEffect} from 'react'
 import './App.css'
 import Header from './Components/Header'
 import AboutMe from './Components/AboutMe'
@@ -7,18 +7,37 @@ import Projects from './Components/Projects'
 import ContactMe from './Components/ContactMe'
 import Footer from './Components/Footer'
 
-
 function App() {
   const [headerHeight, setHeaderHeight] = useState(0);
+  const [isToggleOpen, setIsToggleOpen] = useState(false)
+  useEffect(() => {
+    const handleResize = () => {
+      const header = document.getElementById('header');
+      if(isToggleOpen){
+        setHeaderHeight(280)
+      }
+      else{
+        setHeaderHeight(90)
+      }
+    };
 
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [isToggleOpen]);
   return (
     <div id="whole_page">
-    <Header setHeaderHeight={setHeaderHeight}/>
+    <Header setHeaderHeight={setHeaderHeight} setIsToggleOpen={setIsToggleOpen} isToggleOpen={isToggleOpen}/>
+
+
     <section id="mainBody" style={{ marginTop: headerHeight }}>
-    <div id="aboutMe_div" class="mainDiv"><AboutMe/></div>
-    <div id="skills_ed_div" class="mainDiv"> <SkillsAndEducation/></div>
-    <div id="projects_div" class="mainDiv"> <Projects/></div>
-    <div id="contactMe_div" class="mainDiv"><ContactMe/></div>
+    <div id="aboutMe_div" className="mainDiv"><AboutMe/></div>
+    <div id="skills_ed_div" className="mainDiv"> <SkillsAndEducation/></div>
+    <div id="projects_div" className="mainDiv"> <Projects/></div>
+    <div id="contactMe_div" className="mainDiv"><ContactMe/></div>
     </section>
     <Footer/>
 </div>
--- my-react-app/src/Components/Header.jsx
+++ my-react-app/src/Components/Header.jsx
@@ -1,44 +1,34 @@
 import { Navbar, Nav } from "react-bootstrap";
 import React, { useEffect, useRef, useState } from "react";
 
-function Header({ setHeaderHeight }) {
+function Header({ setHeaderHeight, setIsToggleOpen, isToggleOpen }) {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const headerRef = useRef(null);
 
-  useEffect(() => {
-    const handleResize = () => setWindowWidth(window.innerWidth);
-
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  useEffect(() => {
-    const updateHeaderHeight = () => {
-      if (headerRef.current) {
-        setHeaderHeight(headerRef.current.offsetHeight);
-      }
-    };
-
-    updateHeaderHeight();
-
-    window.addEventListener("resize", updateHeaderHeight);
-
-    return () => window.removeEventListener("resize", updateHeaderHeight);
-  }, [setHeaderHeight]);
 
   const handleNavClick = (event, targetId) => {
     event.preventDefault();
     const targetElement = document.getElementById(targetId);
-    const headerOffset = document.getElementById("header").offsetHeight;
+    const headerOffset = headerRef.current.offsetHeight
     const elementPosition =
       targetElement.getBoundingClientRect().top + window.scrollY;
     const offsetPosition = elementPosition - headerOffset;
 
-    window.scrollTo({
-      top: offsetPosition,
-      behavior: "smooth",
-    });
-  };
+   
+      window.scrollTo({
+        top: offsetPosition,
+      });
+
+};
+
+const handleToggle =() =>{
+if(isToggleOpen){
+  setIsToggleOpen(false)
+}
+else{
+  setIsToggleOpen(true)
+}
+}
 
   return (
     <section>
@@ -78,9 +68,11 @@ function Header({ setHeaderHeight }) {
         </Navbar>
       )}
       {windowWidth <= 1000 && (
-        <Navbar id="header" ref={headerRef} fixed="top" data-bs-theme="dark" expand='lg'>
+        <Navbar id="header" ref={headerRef} fixed="top" data-bs-theme="dark" expand='lg'  
+        onToggle={handleToggle} expanded={isToggleOpen}
+        >
           <div id="myName">
-            <Navbar.Brand href="#home">CATHERINE PULMAN</Navbar.Brand>
+            <Navbar.Brand href="#home"><p>CATHERINE PULMAN</p></Navbar.Brand>
             <Navbar.Toggle
               aria-controls="basic-navbar-nav"
               className="toggle-custom"
@@ -119,6 +111,55 @@ function Header({ setHeaderHeight }) {
         </Navbar>
       )}
     </section>
+  //   <section>
+  //   <Navbar
+  //     id="header"
+  //     ref={headerRef}
+  //     fixed="top"
+  //     data-bs-theme="dark"
+  //     expand="lg"
+  //     onToggle={handleToggle}
+  //     expanded={isToggleOpen}
+  //   >
+  //     <div id="myName">
+  //       <Navbar.Brand href="#home">CATHERINE PULMAN</Navbar.Brand>
+  //       <Navbar.Toggle
+  //         aria-controls="basic-navbar-nav"
+  //         className="toggle-custom"
+  //       />
+  //     </div>
+  //     <Navbar.Collapse id="basic-navbar-nav">
+  //       <Nav className="me-auto">
+  //         <Nav.Link
+  //           href="#aboutMe_div"
+  //           onClick={(e) => handleNavClick(e, "aboutMe_div")}
+  //         >
+  //           About Me
+  //         </Nav.Link>
+  //         <Nav.Link
+  //           href="#skills_ed_div"
+  //           onClick={(e) => handleNavClick(e, "skills_ed_div")}
+  //         >
+  //           Skills & Education
+  //         </Nav.Link>
+  //         <Nav.Link
+  //           href="#projects_div"
+  //           onClick={(e) => handleNavClick(e, "projects_div")}
+  //         >
+  //           Projects
+  //         </Nav.Link>
+  //         <Nav.Link
+  //           href="#contactMe_div"
+  //           onClick={(e) => handleNavClick(e, "contactMe_div")}
+  //         >
+  //           Contact Me
+  //         </Nav.Link>
+  //       </Nav>
+  //     </Navbar.Collapse>
+  //   </Navbar>
+  // </section>
+
+
   );
 }
 export default Header;
